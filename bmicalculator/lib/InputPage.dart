9493307b44efe9fda0a3bfeaf42ae10a934acfcd
soorajkumarluhana <soorajkumarluhana@gmail.com>
2022-04-04 19:14:11 +0500

    done

--- bmicalculator/lib/InputPage.dart
+++ bmicalculator/lib/InputPage.dart
@@ -1,18 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'IconContent.dart';
 import 'Reusable.dart';
+import 'constant.dart';
 
-const bottomContainerHeight = 80.0;
-const activeCardColour = Color(0xFF1D1E33);
-const bottomContainerColour = Color(0xFFEB1555);
-const inactiveCardColour = Color(0xFF111328);
+enum Gender {
+  male,
+  female,
+}
 
-Color maleCardColour = inactiveCardColour;
-Color femaleCardColour = inactiveCardColour;
+int Height = 180;
+int Weight = 60;
 
-void updateColour(int gender) {
-  if (gender == 1) {
+void updateColour(Gender SelectedGender) {
+  if (SelectedGender == Gender.male) {
     if (maleCardColour == inactiveCardColour) {
       maleCardColour = activeCardColour;
       femaleCardColour = inactiveCardColour;
@@ -21,8 +24,8 @@ void updateColour(int gender) {
     }
   }
 
-  if (gender == 2) {
-    if (femaleCardColour == inactiveCardColour) {
+  if (SelectedGender == Gender.female) {
+    if (SelectedGender == Gender.female) {
       femaleCardColour = activeCardColour;
       maleCardColour = inactiveCardColour;
     } else {
@@ -47,6 +50,7 @@ class _InputPageState extends State<InputPage> {
           title: Text("BMI Calculator"),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Expanded(
                 child: Row(
@@ -55,7 +59,7 @@ class _InputPageState extends State<InputPage> {
                     child: GestureDetector(
                   onTap: () {
                     setState(() {
-                      updateColour(1);
+                      updateColour(Gender.male);
                     });
                   },
                   child: ReUsableCode(
@@ -70,7 +74,7 @@ class _InputPageState extends State<InputPage> {
                     child: GestureDetector(
                   onTap: () {
                     setState(() {
-                      updateColour(2);
+                      updateColour(Gender.female);
                     });
                   },
                   child: ReUsableCode(
@@ -86,7 +90,38 @@ class _InputPageState extends State<InputPage> {
             Expanded(
                 child: ReUsableCode(
               Colour: activeCardColour,
-              cardChild: Text("s"),
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("HEIGHT", style: klabelTextStyle),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        Height.toString(),
+                        style: kNumberTextStyle,
+                      ),
+                      Text(
+                        "cm",
+                        style: klabelTextStyle,
+                      )
+                    ],
+                  ),
+                  Slider(
+                      value: Height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      activeColor: Color(0xFFEB1555),
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (double newvalue) {
+                        setState(() {
+                          Height = newvalue.round();
+                        });
+                      })
+                ],
+              ),
             )),
             Expanded(
                 child: Row(
@@ -94,7 +129,26 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                     child: ReUsableCode(
                   Colour: activeCardColour,
-                  cardChild: Text("s"),
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Weight", 
+                        style: klabelTextStyle,
+                      ),
+                    Text(Weight.toString(),
+                    style: kNumberTextStyle,
+                    ),
+                  Row(
+                  mainAxisAlignment: MainAxisAlignment.center,  
+                    children: [
+                   FloatingActionButton(onPressed: (){}, child: Icon(Icons.add),
+                   
+                   )   
+                    ],
+                  )  
+                    ],
+                  ),
                 )),
                 Expanded(
                     child: ReUsableCode(
---
+++ bmicalculator/lib/constant.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+const inactiveCardColour = Color(0xFF111328);
+
+Color maleCardColour = inactiveCardColour;
+Color femaleCardColour = inactiveCardColour;
+
+const klabelTextStyle = TextStyle(fontSize: 18.0, color: Color(0XFF8D8E98));
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900
+);
+
+
